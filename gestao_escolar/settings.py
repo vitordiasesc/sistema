a44b6517e82846476a186144455337f35fdcd906
Alexandre Martins Vieira <alexandre@amv.tec.br>
2025-04-02 16:04:58 -0300

    Corrige arquivos estáticos do admin Django

--- gestao_escolar/settings.py
+++ gestao_escolar/settings.py
@@ -129,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Em produção, certifique-se de ter isso também:
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
