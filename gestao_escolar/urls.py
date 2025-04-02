5270d4b4da7a90cc3222a7bac7c7409b6c778e62
Alexandre Martins Vieira <alexandre@amv.tec.br>
2025-04-02 15:56:57 -0300

    alteracao em urls

--- gestao_escolar/urls.py
+++ gestao_escolar/urls.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static  # 👈 Import necessário
-from .views import criar_superuser
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('alunos/', include('alunos.urls')),
-    path('criar-superuser/', criar_superuser, name='criar_superuser'),
 ]
 
 # 👇 Adicione isso para servir arquivos de mídia durante o desenvolvimento
